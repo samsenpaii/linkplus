29a99cce4784d865ec846e8679e39b05d1a351d7
Samarth <samarthpatil2021.comp@mmcoe.edu.in>
2025-04-29 01:19:10 +0530

    stage 1 - progress Navbar ready

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -2,15 +2,18 @@ import Navbar from "@/components/Navbar/Navbar"
 
 function page() {
   return (
-    <div className=" h-screen w-screen flex">
-        <div className="bg-[#F6F5F4] w-96 h-screen border-2">
-            <h1>nice</h1>
-        </div>
-        <div className="bg-asblack h-screen ">
-           <Navbar></Navbar>
-        </div>
+    <div className="h-screen w-screen flex">
+      {/* Sidebar */}
+      <div className="bg-[#F6F5F4] w-96 h-screen border-2">
+        <h1>nice</h1>
+      </div> 
+
+      {/* Main Content Area */}
+      <div className="h-screen flex-1 flex flex-col">
+        <Navbar />
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,48 @@
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-  } from "@/components/ui/navigation-menu"
+import BookmarkIcon from "../icons/bookmarkIcon";
+import ClockIcon from "../icons/clockIcon";
+import DownArrowIcon from "../icons/downarrowIcon";
+import PlusIcon from "../icons/plusIcon";
+import SearchIcon from "../icons/searchIcon";
 
-  
-function Navbar() {
+export default function Navbar() {
   return (
-    <NavigationMenu>
-        <NavigationMenuList>
-            <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-                <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-            </NavigationMenuItem>
-        </NavigationMenuList>
-    </NavigationMenu>
-  )
-}
+    <>
+      <div className="flex flex-col font-sans border-b-[1px] p-2">
+        {/* Top Navbar */}
+        <div className="m-2 flex flex-row items-center justify-between">
+          <div className="bg-[#F6F5F4] p-1 w-72 flex items-center rounded-sm">
+            <div className="flex items-center justify-center p-2">
+              <SearchIcon style="text-[#4f4c4c]" />
+            </div>
+            <input
+              type="text"
+              placeholder="Search"
+              className=" text-[#636161] bg-[#F6F5F4] outline-none flex-1"
+            />
+          </div>
 
-export default Navbar
+          <div className="bg-blue-500 rounded-sm p-1 flex items-center justify-center text-[#F6F5F4] hover:cursor-pointer">
+            <PlusIcon />
+            <div className="pr-2 pl-2">Add link</div>
+          </div>
+        </div>
+
+        {/* Bookmark Section */}
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row items-center space-x-2">
+            <BookmarkIcon style="text-[#4f4c4c]"/>
+            <div className="text-2xl text-[#4f4c4c]">All BookMarks</div>
+          </div>
+          <div className="flex flex-row justify-center items-center hover:bg-[#f2efef] rounded-sm p-1 cursor-pointer">
+            <ClockIcon style="text-[#4f4c4c]"/>
+            <div className="text-[#4f4c4c] pl-2">
+              Sort by Time
+            </div>
+            <DownArrowIcon style="text-[#4f4c4c]"></DownArrowIcon>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
